a9067a798906584bf77d2011e7dd1ca3811b6934
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2025-10-16 00:55:00 +0600

    array

--- TrappingRainWaterForLoop.java
+++ TrappingRainWaterForLoop.java
@@ -23,7 +23,6 @@ public class TrappingRainWaterForLoop {
         }
            return total;
 
-        
     }
     public static void main(String[] args) {
          int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
